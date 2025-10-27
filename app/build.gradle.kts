82b781f73b35ea03f2ed61a7d99f580ba8daf1b1
서정우 <tjwjddn1025@naver.com>
2025-10-27 13:25:15 +0900

    feat. fragment_register_calendar.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     // 캘린더 커스텀
     implementation("com.jakewharton.threetenabp:threetenabp:1.1.1")
-    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("com.kizitonwose.calendar:view:2.5.4")
 
 
     //구글 맵
