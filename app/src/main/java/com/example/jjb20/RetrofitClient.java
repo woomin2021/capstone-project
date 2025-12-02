ae0f87ed07eae3f8755b340ea984a6d90537383b
tjwjddn <tjwjddn1025@naver.com>
2025-12-02 13:53:38 +0900

    fix. ratingbar 색상 변경, HouseReviewActivity.java 정보 불러오기 수정

--- app/src/main/java/com/example/jjb20/RetrofitClient.java
+++ app/src/main/java/com/example/jjb20/RetrofitClient.java
@@ -17,8 +17,8 @@ public class RetrofitClient {
     //retrofit 연결ㄴ
     private static Retrofit retrofit;
     //현재 서버에 스프링을 안올려놨기 때문에 10.0.2.2 사용
-    // TODO 서버에 스프링 올릴시 223.130.130,141로 교체 해야함
-    private static final String BASE_URL = "http://223.130.130,141:8080/";
+    // TODO 서버에 스프링 올릴시 223.130.130.141로 교체 해야함
+    private static final String BASE_URL = "http://223.130.130.141:8080/";
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
